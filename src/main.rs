aef80348d0b5aae8bc018d598130da67f2af8ecb
Samuel Åkesson <samak519@student.liu.se>
2026-01-10 21:54:28 +0100

    WIP:

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-// course info https://cloud.timeedit.net/liu/web/schema/objects/{OBJECT_ID}/o.json?fr=t gives JSON
+// FROM OBJECT ID TO string -> course info https://cloud.timeedit.net/liu/web/schema/objects/{OBJECT_ID}/o.json?fr=t gives JSON 
+// get everything in JSON https://cloud.timeedit.net/liu/web/schema/objects//o.json?fr=t gives JSON 
 //  schema genrerator: https://cloud.timeedit.net/liu/web/schema/sid=3
 
 use dioxus::prelude::*;
@@ -10,24 +11,18 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    let mut response_contents = use_signal(|| "wtf".to_string());
-
-    let fetch = move |_| async move {
-        info!("did");
+    let fetch = use_resource(|| async {
         const YEAR: &str = "2026";
         let object_id_string = "153444";
-        let response = reqwest::get(format!("https://cloud.timeedit.net/liu/web/schema/ri.ics?part=t&sid=3&p={YEAR}0101%2C{YEAR}1231&objects={object_id_string}")).await.unwrap().text().await.unwrap();
-        response_contents.set(response);
-    };
+        let response = reqwest::get(format!("https://cloud.timeedit.net/liu/web/schema/objects//o.json?fr=f&types=205&sid=3")).await.unwrap().text().await.unwrap();
+        response
+    });
 
     rsx! {
         document::Link { rel: "icon", href: asset!("/assets/logo.svg") }
         document::Link { rel: "stylesheet", href: asset!("/assets/style.css") }
-        input {
-            oninput: fetch
-        }
         div {
-            {response_contents}
+            {fetch}
         }
     }
 }
