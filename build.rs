ad273a188036423fc897ae0467acf6f7de8e1192
Samuel Åkesson <samak519@student.liu.se>
2026-01-11 12:14:08 +0100

    chore: restructure project

--- build.rs
+++
@@ -1,8 +0,0 @@
-use std::error::Error;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let objects = reqwest::blocking::get(
-        "https://cloud.timeedit.net/liu/web/schema/objects//o.json?fr=f&types=205&sid=3",
-    )?;
-    Ok(())
-}
