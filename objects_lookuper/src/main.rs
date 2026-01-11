39975bda77dd714c273d8c54c276f0a03e820372
Samuel Åkesson <samak519@student.liu.se>
2026-01-11 17:43:01 +0100

    feat: add both studentgroup and course type

--- objects_lookuper/src/main.rs
+++ objects_lookuper/src/main.rs
@@ -1,51 +1,46 @@
 use color_eyre::Result;
-use serde::Deserialize;
 use std::fs::File;
-use std::io::Write;
-use wincode::{SchemaRead, SchemaWrite};
+use std::io::{Write, stdout};
 
-#[derive(Deserialize, Debug, SchemaWrite, SchemaRead)]
-struct ObjectRecord {
-    id: u32,
-    values: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct ObjectSearchResponse {
-    count: u16,
-    records: Vec<ObjectRecord>,
-}
+use types::{ObjectSearchResponse, ObjectType};
 
 fn main() -> Result<()> {
     let client = reqwest::blocking::Client::new();
-
     const MAX: u16 = 100;
-    let mut start = 0;
 
     let mut all_object_records = Vec::new();
+    let mut stdout_lock = stdout().lock();
+    for object_type in [ObjectType::StudentGroup, ObjectType::Course] {
+        println!("doing {:?}", &object_type);
+        let object_type_string = (object_type as i32).to_string();
+        let mut start = 0;
+        loop {
+            let raw_response = client
+                .get("https://cloud.timeedit.net/liu/web/schema/objects/o.json")
+                .query(&[
+                    ("types", object_type_string.as_str()),
+                    ("sid", "3"),
+                    ("max", &MAX.to_string()),
+                    ("start", &start.to_string()),
+                ])
+                .send()?;
 
-    loop {
-        let mut response: ObjectSearchResponse = client
-            .get("https://cloud.timeedit.net/liu/web/schema/objects/o.json")
-            .query(&[
-                ("types", "205"),
-                ("sid", "3"),
-                ("max", &MAX.to_string()),
-                ("start", &start.to_string()),
-            ])
-            .send()?
-            .json()?;
+            writeln!(stdout_lock, "fetching {}", raw_response.url())?;
 
-        all_object_records.append(&mut response.records);
+            let mut response: ObjectSearchResponse = raw_response.json()?;
 
-        println!("fetched {}-{}", start, start + response.count);
-        if response.count < MAX {
-            break;
+            if response.count == 0 {
+                break;
+            }
+
+            all_object_records.append(&mut response.records);
+
+            writeln!(stdout_lock, "fetched {}-{}", start, start + response.count)?;
+            start += MAX;
         }
-        start += MAX;
     }
 
-    let mut file = File::create("records.bin")?;
+    let mut file = File::create("../web/assets/records.bin")?;
     file.write_all(&wincode::serialize(&all_object_records)?)?;
 
     Ok(())
---
+++ types/src/lib.rs
@@ -0,0 +1,24 @@
+use serde::Deserialize;
+use serde_repr::Deserialize_repr;
+use wincode::{SchemaRead, SchemaWrite};
+
+#[derive(Deserialize, Debug)]
+pub struct ObjectSearchResponse {
+    pub count: u16,
+    pub records: Vec<ObjectRecord>,
+}
+
+#[derive(Deserialize, Debug, SchemaWrite, SchemaRead, PartialEq)]
+pub struct ObjectRecord {
+    pub id: u32,
+    pub values: String,
+    #[serde(rename = "typeId")]
+    pub r#type: ObjectType,
+}
+
+#[derive(Deserialize_repr, Debug, SchemaWrite, SchemaRead, PartialEq)]
+#[repr(i32)]
+pub enum ObjectType {
+    StudentGroup = 205,
+    Course = 219,
+}
